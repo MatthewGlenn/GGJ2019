360862a1a1aa0a117a020d344462b008063df45e
Ben Johnson <bfjdevmail@gmail.com>
2019-01-26 17:00:23 -0500

    Added PlayCharacter prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,21 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb2D;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
+    private Animator animator;      //Stores the animation controller for the player
+    private BoxCollider2D boxCollider;
+
     // Use this for initialization
     void Start()
     {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        //Get and store a references to components
         rb2D = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
