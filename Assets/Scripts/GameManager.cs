7b71f47e9ce37dd3e729f058a1a99c8d8f5699e5
John Poe <jmpoe2@ncsu.edu>
2019-01-26 17:16:53 -0500

    basic log file for keydown

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    private GameObject levelImage;
+
+    void Awake() {
+        if(instance == null)
+            instance = this;
+        else if(instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        
+        InitGame();
+    }
+
+     void InitGame()
+    {
+        levelImage = GameObject.Find("LevelImage");
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("The left key was pressed");
+        }
+        else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            Debug.Log("The right key was pressed");
+        }
+    }
+
+}
