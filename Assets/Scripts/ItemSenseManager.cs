f03c91af4865436be48067427c7abf0a1cbefc4a
Reese Thompson <ReeseSThomp@gmail.com>
2019-01-27 13:41:27 -0500

    Fixed char choice.

--- Assets/Scripts/ItemSenseManager.cs
+++ Assets/Scripts/ItemSenseManager.cs
@@ -6,7 +6,7 @@ public class ItemSenseManager : MonoBehaviour
 {
     private Transform _itemTransform;
 
-    public GameObject playerObject;
+    private GameObject[] playerObjects;
     public GameObject audioObject;
     private AudioSource _GOAudioSource;
     // Start is called before the first frame update
@@ -14,16 +14,26 @@ public class ItemSenseManager : MonoBehaviour
     {
         _itemTransform = GetComponent<Transform>();
         _GOAudioSource = audioObject.GetComponent<AudioSource>();
+
+        playerObjects = GameObject.FindGameObjectsWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(_itemTransform.position, playerObject.transform.position);
-        
-        if (distance < 5.0f) {
-            float volume = (100 - (distance * 20.0f)) / 100;
-            _GOAudioSource.volume = volume;
+        if (playerObjects.Length > 1) {
+            playerObjects = GameObject.FindGameObjectsWithTag("Player");
         }
+        
+
+        foreach ( var playerObject in playerObjects) {
+            float distance = Vector3.Distance(_itemTransform.position, playerObject.transform.position);
+        
+            if (distance < 5.0f) {
+                float volume = (100 - (distance * 20.0f)) / 100;
+                _GOAudioSource.volume = volume;
+            }
+        }
+        
     }
 }
